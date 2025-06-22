944d321c69f3d89af865b4386893ef25809f1224
plooserloser <pluschatkul@gmail.com>
2025-06-22 03:24:43 +0000

    multiplication

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+print("enter a number")
+number_entered=input()
+if int(number_entered) < 0 :
+        print("this number is negative")
+if int(number_entered)> 0 :
+        print("this number is positive")
+if int(number_entered) == 0 :
+            print("this number is both negative and positive")
+
---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "789"
+print("Please enter your password")
+password_enetered=input()
+if password== password_enetered :
+    print("ACCESS GRANTED") 
+else:
+        print("ACCESS DENIED")
