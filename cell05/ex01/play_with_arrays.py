d84961f2f748049744ee8f9334fd0d866e5a3913
plooserloser <pluschatkul@gmail.com>
2025-06-22 08:24:59 +0000

    array demon

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,6 @@
+my_list = [ 2, 8, 9, 48, 8, 22, -12, 2]
+my_list_plus_two = []
+for number in my_list:
+        my_list_plus_two.append(number+2)
+print("original array", my_list)
+print("New array:" , my_list_plus_two)
