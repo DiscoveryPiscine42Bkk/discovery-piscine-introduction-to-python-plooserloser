2dd5f16f9aa52f11593c82ab355ca2819774b82c
plooserloser <pluschatkul@gmail.com>
2025-06-22 08:47:56 +0000

    arrays pt.2

---
+++ cell05/ex02/play_with_arrays++.py
@@ -0,0 +1,7 @@
+original_list = [2, 8, 9, 48, 8, 22,-12, 2]
+filtered_list = []
+for x in original_list:
+    if x >= 5:
+        filtered_list.append(x+2)
+print(original_list)
+print(filtered_list)
