3b831ace3df445ac806f0fd883fccfd7bb57d4d3
plooserloser <pluschatkul@gmail.com>
2025-06-24 04:24:05 +0000

    FixCell01Ex01

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-first_name= 'Chatkul'
-last_name= 'Pawboonserm'
-whole_name=first_name,last_name 
+first_name = "Chatkul"
+last_name = "Pawboonserm"
+whole_name = first_name + " " + last_name
 print(whole_name)
